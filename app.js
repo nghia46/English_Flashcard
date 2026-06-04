d1b9cde9fe326e66019cd7f1a27ca903633f80a8
nghia46 <nghialoe46@gmail.com>
2026-06-04 16:52:29 +0700

    update worlds

--- app.js
+++ app.js
@@ -522,7 +522,7 @@ function loadProgressFromStorage() {
 
             const keys = Object.keys(topicsData);
             if (keys.length > 0) {
-                fileLabel.textContent = `💾 Đã khôi phục tiến trình học tập từ bộ nhớ máy`;
+                //fileLabel.textContent = `💾 Đã khôi phục tiến trình học tập từ bộ nhớ máy`;
                 const activeTopic = (storedCurrent && keys.includes(storedCurrent)) ? storedCurrent : keys[0];
                 updateSelectDropdown(activeTopic);
                 return true;
